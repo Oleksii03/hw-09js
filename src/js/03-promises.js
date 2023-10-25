c7755d8f5baba4f9dc088e8a6171220de51781b2
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-10-25 12:16:29 +0300

    fix promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form-js');
+
+function createPromise (position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+
+};
+
+
+formEl.addEventListener('submit', onCreatePromise);
+
+function onCreatePromise (e) {
+  e.preventDefault();
+  const { target } = e;
+
+  const delay = Number(target.elements.delay.value);
+  const step = Number(target.elements.step.value);
+  const amount = Number(target.elements.amount.value);
+
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay);
+  }
+
+  target.reset();
 }
